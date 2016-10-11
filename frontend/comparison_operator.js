36aff5e75556e70f6690add9ad7ee6b9192d52dc
Rutul Dave <rutul98@yahoo.com>
2016-10-11 14:56:06 -0400

    difference between == and ===

---
+++ frontend/comparison_operator.js
@@ -0,0 +1,10 @@
+/* What is the difference between '==' and '===' comparison operators? */
+
+var A = '0';
+var B = 0;
+var C = '';
+
+console.log(A == B);
+console.log(A === B);
+console.log(B == C);
+console.log(A == C);
